82f19b6c16b37531c1ad8cf7961bbaeddaf5b682
Martin Lejko <martinlejko@Matkos-MacBook-Air.local>
2023-10-15 16:07:26 +0200

    Added files from the first exercise

--- practicals1/practicals1/Program.cs
+++ practicals1/practicals1/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Specialized;
-using System.Collections.Generic;
-using System.Diagnostics;
+
 using System.IO;
 class Program {
     static void Main(string[] args) {
