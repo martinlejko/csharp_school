9844c9e224e8551544fc33e1fdf6860631f13181
Martin Lejko <Martin.lejko@outlook.com>
2023-12-08 19:29:07 +0100

    Added some unittets

--- practicals6/nezarka_tests/UnitTest1.cs
+++ practicals6/nezarka_tests/UnitTest1.cs
@@ -1,32 +1,103 @@
 using System;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using Xunit;
 
 public class UnitTest1
 {
     [Fact]
-    public void TestConsoleInputOutput()
+    public void TestInvalidOutput()
+    {
+        string htmlContent = "<!DOCTYPE html>\n<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n\t<meta charset=\"utf-8\" />\n\t<title>Nezarka.net: Online Shopping for Books</title>\n</head>\n<body>\n<p>Invalid request.</p>\n</body>\n</html>\n====";
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        ModelStore bookstoreModel;
+		View bookstoreView;
+		Controller controller;
+        bookstoreModel = new ModelStore();
+        controller = new Controller(bookstoreModel);
+
+        var reader = new StreamReader("data.txt");
+        bookstoreView = new View(bookstoreModel, controller, reader);
+        bookstoreView.CallInvalidRequest();
+        var output = writer.GetStringBuilder().ToString().Trim();
+        Assert.Equal(htmlContent, output);
+       
+    }
+
+    [Fact]
+    public void TestHeaderOutput(){
+        string htmlContent = "<!DOCTYPE html>\n" +
+        "<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+        "<head>\n" +
+        "\t<meta charset=\"utf-8\" />\n" +
+        "\t<title>Nezarka.net: Online Shopping for Books</title>\n" +
+        "</head>\n" +
+        "<body>\n" +
+        "\t<style type=\"text/css\">\n" +
+        "\t\ttable, th, td {\n" +
+        "\t\t\tborder: 1px solid black;\n" +
+        "\t\t\tborder-collapse: collapse;\n" +
+        "\t\t}\n" +
+        "\t\ttable {\n" +
+        "\t\t\tmargin-bottom: 10px;\n" +
+        "\t\t}\n" +
+        "\t\tpre {\n" +
+        "\t\t\tline-height: 70%;\n" +
+        "\t\t}\n" +
+        "\t</style>\n" +
+        "\t<h1><pre>  v,<br />Nezarka.NET: Online Shopping for Books</pre></h1>\n" +
+        "\t" + "John"+ ", here is your menu:\n" +
+        "\t<table>\n" +
+        "\t\t<tr>\n" +
+        "\t\t\t<td><a href=\"/Books\">Books</a></td>\n" +
+        "\t\t\t<td><a href=\"/ShoppingCart\">Cart (" + "0" + ")</a></td>\n" +
+        "\t\t</tr>\n" +
+        "\t</table>";
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        ModelStore bookstoreModel;
+        View bookstoreView;
+        Controller controller;
+        bookstoreModel = new ModelStore();
+        bookstoreModel.customers.Add(new Customer {Id = 1, FirstName = "John", LastName = "Doe"});
+        int customerId = 1;
+        controller = new Controller(bookstoreModel);
+        bookstoreView = new View(bookstoreModel, controller, new StreamReader("data.txt"));
+        bookstoreView.CallHeader(customerId);
+        var output = writer.GetStringBuilder().ToString().Trim(); 
+        Assert.Equal(htmlContent, output);
+    }
+
+    [Fact]
+    public void Process_ValidRequest_ReturnsExpectedResult()
     {
         // Arrange
-        string testData = "DATA-BEGIN\nBOOK;1;Book Title;Author;25.99\nDATA-END";
-        File.WriteAllText("data.txt", testData);
-
-        string expectedOutput = "EXPECTED OUTPUT";
-
-        using (StringWriter sw = new StringWriter())
+        string[] lines = new string[]
         {
-            using (StringReader sr = new StringReader(testData))
-            {
-                Console.SetOut(sw);
-                Console.SetIn(sr);
+            "GET 123 https://example.com/customers/123/orders",
+            // Add more test cases here if needed
+        };
+        string htmlContent = "<!DOCTYPE html>\n<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n\t<meta charset=\"utf-8\" />\n\t<title>Nezarka.net: Online Shopping for Books</title>\n</head>\n<body>\n<p>Invalid request.</p>\n</body>\n</html>\n====";
+        // Set up a StringReader to simulate the behavior of the reader object
+        using (StringReader reader = new StringReader(string.Join(Environment.NewLine, lines)))
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            ModelStore bookstoreModel = new ModelStore();
+            Controller controller = new Controller(bookstoreModel);
+            View bookstoreView = new View(bookstoreModel, controller, reader);
 
-                // Act
-                Porgram.Main();
+            var output = writer.GetStringBuilder().ToString().Trim();
+            // Act
+            bookstoreView.Process();
 
-                // Assert
-            string actualOutput = sw.ToString().Trim();
-            Assert.Equal(expectedOutput, actualOutput);
+        
+            Assert.Equal(htmlContent, output);
         }
     }
-}
+
+
+
 }
