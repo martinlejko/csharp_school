077aa8d7c2e23b58091e5f9f064ed880dff9bdeb
Martin Lejko <Martin.lejko@outlook.com>
2024-01-20 00:57:07 +0100

    w/e

---
+++ rychlovky/NullTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+struct S3 {
+public int X;
+public void QuadrupleX() {
+X *= 4;
+}
+}
+
+class Program {
+    static void Main() {
+        S3? s = new S3 { X = 10 };
+        Console.WriteLine(sizeof(S3));
+    }
+}
+
---
+++ rychlovky/testicek.py
@@ -0,0 +1,8 @@
+a = "Vytvoˇruj ́ıc ́ı funkce, Catalanova ˇc ́ısla, projektivn ́ı rovina, ˇr ́ad koneˇcn ́e projektivn ́ı roviny, hypergraf, incidenˇcn ́ı graf hypergrafu, dualita projektivn ́ıch rovin, tokov ́a s ́ıt’, tok, velikost toku, maxim ́aln ́ı tok, ˇrez v s ́ıti, kapacita ˇrezu, element ́arn ́ı ˇrez, minim ́aln ́ı ˇrez, nenasycen ́a a zlepˇsuj ́ıc ́ı cesta, p ́arov ́an ́ı v grafu, vrcholov ́e pokryt ́ı v grafu, syst ́em r ̊uzny ́ch reprezentant ̊u v hypergrafu, hranovy ́ a vrcholovy ́ ˇrez v grafu, hranov ́a a vrcholov ́a souvislost grafu, hranovˇe a vrcholovˇe k-souvisly ́ graf, klika a nez ́avisl ́a mnoˇzina v grafu, Hammingova vzd ́alenost a Hammingova v ́aha, minim ́aln ́ı vzd ́alenost k ́odu, (n, k, d)-k ́od, line ́arn ́ı k ́od, generuj ́ıc ́ı matice, k ́odov ́an ́ı, dek ́odov ́an ́ı, du ́aln ́ı k ́od, kontroln ́ı matice line ́arn ́ıho k ́odu, Hammin- govy k ́ody"
+
+b = a.replace(" ́", "́")
+c = b.replace(" ̊", "̊")
+d = c.replace(" ́", "́")
+e = d.replace(" ́", "́")
+f = e.split(", ")
+print(len(f))
